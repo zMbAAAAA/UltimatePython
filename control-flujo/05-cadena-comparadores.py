f739bb2806fc0251794ce26544f14992624083b8
zMbAAAAA <zaambaaa@gmail.com>
2024-07-20 19:15:22 +0200

    pythoNot

---
+++ control-flujo/05-cadena-comparadores.py
@@ -0,0 +1,8 @@
+edad = 25
+
+if 15 <= edad <= 65:
+
+    # if edad >= 15 and edad <= 65:
+    #     print("Puede entrar a la piscina")
+    # else:
+    print("No puede entrar")
---
+++ control-flujo/06-for.py
@@ -0,0 +1,1 @@
+for numero in range(5):
